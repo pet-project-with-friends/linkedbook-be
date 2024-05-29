19bbb599fa33fef148e067e3d5c59a7e10bdd0c9
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2024-05-30 03:28:54 +0700

    feat: import auth and post modules

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
+
+@Module({
+  imports: [AuthModule, PostModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
