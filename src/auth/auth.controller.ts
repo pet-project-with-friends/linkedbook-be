40162d68a9248065955451f5c28957a7a87e88f2
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2024-05-30 05:30:50 +0700

    feat: add description for swagger ui

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,7 +16,13 @@ import { JwtResponse, RefreshTokenRequest } from 'src/model/token.model';
 import { AuthGuard } from './auth.guard';
 import { ValidateUsername } from 'src/model/user.model';
 import { Response } from 'express';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('auth')
 @ApiTags('Authentication')
@@ -25,11 +31,38 @@ export class AuthController {
 
   @Post('login')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Login account' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        identity: { type: 'string', example: 'username | email' },
+        password: { type: 'string', example: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 200 })
+  @ApiResponse({ status: 404, description: 'Email or username incorrect.' })
+  @ApiResponse({ status: 400, description: 'Password incorrect!' })
   async login(@Body() data: LoginRequest): Promise<JwtResponse> {
     return this.authService.login(data);
   }
 
   @Post('register')
+  @ApiOperation({ summary: 'Regist new account' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', example: 'string' },
+        username: { type: 'string', example: 'string' },
+        password: { type: 'string', example: 'string' },
+        usernaconfirmPasswordme: { type: 'string', example: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Register successfully!' })
+  @ApiResponse({ status: 400 })
   async register(@Body() data: RegisterRequest) {
     return this.authService.register(data);
   }
@@ -37,6 +70,18 @@ export class AuthController {
   @Post('refreshToken')
   @UseGuards(AuthGuard)
   @HttpCode(200)
+  @ApiOperation({ summary: 'Refesh new token' })
+  @ApiBearerAuth()
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        refreshToken: { type: 'string', example: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 401 })
+  @ApiResponse({ status: 500, description: 'Cannot refresh token.' })
   async refreshToken(
     @Body() request: RefreshTokenRequest,
   ): Promise<JwtResponse> {
@@ -46,6 +91,11 @@ export class AuthController {
   @Delete('revoke')
   @UseGuards(AuthGuard)
   @HttpCode(200)
+  @ApiOperation({ summary: 'Logout and remove token' })
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200 })
+  @ApiResponse({ status: 401, description: 'Token invalid!' })
+  @ApiResponse({ status: 500, description: 'Cannot revoke token.' })
   async revokeToken(@Headers('authorization') token: string) {
     const tokenWithoutPrefix = token.replace('Bearer ', '');
     return this.authService.revokeToken(tokenWithoutPrefix);
@@ -53,6 +103,17 @@ export class AuthController {
 
   @Post('username/validate')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Check existing username' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string', example: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 200 })
+  @ApiResponse({ status: 400 })
   async validateUsername(
     @Body() request: ValidateUsername,
     @Res() res: Response,
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -14,7 +14,14 @@ import {
 import { AuthGuard } from 'src/auth/auth.guard';
 import { PostService } from './post.service';
 import { LikeRequest, PostPagination, PostRequest } from 'src/model/post.model';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('posts')
 @UseGuards(AuthGuard)
@@ -23,6 +30,18 @@ export class PostController {
   constructor(private postService: PostService) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Create new a post' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        title: { type: 'string', example: 'string' },
+        content: { type: 'string', example: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Create successfully!' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   async create(
     @Body() data: PostRequest,
     @Headers('authorization') token: string,
@@ -33,24 +52,52 @@ export class PostController {
 
   @Put('update/:id')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Update a post by id' })
+  @ApiParam({ name: 'id', required: true })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        title: { type: 'string', example: 'string' },
+        content: { type: 'string', example: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 200, description: 'Update successfully!' })
+  @ApiResponse({ status: 404, description: 'Post not found.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   async update(@Param('id') id: string, @Body() data: PostRequest) {
     return this.postService.update(id, data);
   }
 
   @Patch('delete/:id')
   @HttpCode(200)
+  @ApiOperation({ summary: 'Delete a post by id' })
+  @ApiParam({ name: 'id', required: true })
+  @ApiResponse({ status: 200, description: 'Update successfully!' })
+  @ApiResponse({ status: 404, description: 'Post not found.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   async delete(@Param('id') id: string) {
     return this.postService.delete(id);
   }
 
   @Get()
   @HttpCode(200)
+  @ApiOperation({ summary: 'Get all active posts' })
+  @ApiQuery({ name: 'skip' })
+  @ApiQuery({ name: 'limit' })
+  @ApiResponse({ status: 200, description: 'Return paginated list of posts.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   async pagination(@Query() query: PostPagination) {
     return this.postService.getPagination(query);
   }
 
   @Post('like')
   @HttpCode(200)
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Like or unlike a post' })
+  @ApiResponse({ status: 200, description: 'Like or unlike the post.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   async like(
     @Query() query: LikeRequest,
     @Headers('authorization') token: string,
